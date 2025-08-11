1e3601cc5afd9e67fa0c2c89fd13adb3b8f8c901
vasanthrp1812@gmail.com <youremail@example.com>
2025-08-11 15:45:10 +0530

    margin changes for missmatchs

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+  document.addEventListener("click", function(event) {
+  var menu=document.querySelector(".menu-links");
+  var hamburger=document.querySelector(".hamburger-icon");
+  if (!menu.contains(event.target) && !hamburger.contains(event.target))
+    {
+      menu.classList.remove("open");
+      hamburger.classList.remove("open");
+    }
+  });
 
 // THIS IS DISCUSED WITH AI GPT
 // function toggleMenu() {
