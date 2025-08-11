54e751ba82d020ec888cd02a9cc52e5e53f8b0b1
Vasanth <Vasanthrp1812@gmail.com>
2025-08-11 15:49:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,25 +14,3 @@ function toggleMenu() {
     }
   });
 
-// THIS IS DISCUSED WITH AI GPT
-// function toggleMenu() {
-//   const menu = document.querySelector(".menu-links");
-//   const icon = document.querySelector(".hamburger-icon");
-//   menu.classList.toggle("open");
-//   icon.classList.toggle("open");
-// }
-
-// // Show arrow only after scrolling down
-// window.addEventListener("scroll", () => {
-//   const arrow = document.querySelector(".arrow");
-//   if (window.scrollY > 200) {
-//     arrow.classList.add("show");
-//   } else {
-//     arrow.classList.remove("show");
-//   }
-// });
-
-// // Optional: Smooth scroll to top when arrow clicked
-// document.querySelector(".arrow").addEventListener("click", () => {
-//   window.scrollTo({ top: 0, behavior: "smooth" });
-// });
